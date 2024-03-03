2fb1e8d7df1c42d4249075aa283c8350d35c7afb
RileyBeeRBLX <156512500+RileyBeeRBLX@users.noreply.github.com>
2024-03-03 10:10:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var formData = new FormData();
-    formData.append('ipaFile', document.getElementById('ipaFile').files[0]);
-    formData.append('p12File', document.getElementById('p12File').files[0]);
-    formData.append('mobileprovisionFile', document.getElementById('mobileprovisionFile').files[0]);
-
-    fetch('/sign', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            document.getElementById('result').innerHTML = `<a href="${data.downloadLink}" download>Download Signed App</a>`;
-        } else {
-            document.getElementById('result').innerText = data.error;
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+  });
+};
